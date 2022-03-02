1110ff69c23cb39378275e31e696491bc3cd7642
Gene Calmer <yingyangforce@gmail.com>
2022-03-02 18:18:36 -0500

    add header

---
+++ grace_hw4d.py
@@ -0,0 +1,10 @@
+#+-------------------------------+
+#| File: grace_hw4d.py           |
+#| Author: Isaiah Grace          |
+#| Date: 2022/3/2                |
+#| Lab Section: Tuesday          |
+#| Email: isaiah.grace@maine.edu |
+#| Description:                  |
+#| Collabs: N/a                  |
+#+-------------------------------+
+
