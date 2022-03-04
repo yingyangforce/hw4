6c5b5dfeea4444d5cb40bf8e0ed3d8453c377ef3
Gene Calmer <yingyangforce@gmail.com>
2022-03-04 16:47:41 -0500

    finish just in time

--- grace_hw4d.py
+++ grace_hw4d.py
@@ -4,7 +4,16 @@
 #| Date: 2022/3/2                |
 #| Lab Section: Tuesday          |
 #| Email: isaiah.grace@maine.edu |
-#| Description:                  |
+#| Description: squarer          |
 #| Collabs: N/a                  |
 #+-------------------------------+
+
+
+numList = (4,11,18,25,32,39,46,53,60)
+
+def squareNumber(val):
+    return val**2 if val**2 % 2 == 0 else "odd"
+        
+for i in range(len(numList)):
+    print(f"Value {i} is {squareNumber(numList[i])}!")
 
